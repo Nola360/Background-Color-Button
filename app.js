a4f1ad5b1bbac920cd91ed7b0c020d79e22a200a
nola360 <adaramolajr@gmail.com>
2019-12-05 13:22:02 -0500

    added hex color project with html css js code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //target the button and target the body to change the background
 const colorButton = document.querySelector('.color-btn');
+// const bodyBackground
 const bodyBackground = document.querySelector('body');
 
 //Create an Arry to hold background colors to use
@@ -17,3 +18,26 @@ function changeColor() {
   bodyBackground.style.backgroundColor = colors[random];
 }
 
+
+//Hex Project
+
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const hexButton = document.querySelector('.hexButton');
+const hex = document.querySelector('.hex');
+
+hexButton.addEventListener('click', getHex);
+
+function getHex() {
+  let hexColor = '#';
+
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    console.log(random);
+    hexColor += hexNumbers[random];
+    console.log(hexColor);
+  }
+
+  bodyBackground.style.backgroundColor = hexColor;
+  hex.innerHTML = hexColor;
+}
